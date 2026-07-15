684903a46605ae88d6e2fa5c7fbc32910ed5f2fd
bsi26013-spec <bsi26013@uhe.ac.jp>
2026-07-15 14:04:30 +0900

    07/15 for続き

--- 第10回_ネスト/第10回_ネスト/NestBasic.java
+++ 第10回_ネスト/第10回_ネスト/NestBasic.java
@@ -2,11 +2,23 @@
 // マス (i,j) は全部で何個？ 何行？（外3×内2）
 class NestBasic {
     public static void main(String[] args) {
+    	
+    		/*
         for (int i = 0; i < 3; i++) {          // 外側（行）：3回
             for (int j = 0; j < 2; j++) {      // 内側（列）：2回
                 System.out.print("(" + i + "," + j + ") ");
             }
             System.out.println();               // 1行ぶん出したら改行
-        }
-    }
-}
+            }
+         */
+    	
+    	
+			for (int i = 1; i <= 9; i++) {
+				for (int j = 1; j <= 9; j++) {
+					System.out.print(i * j + " ");
+				}
+				System.out.println();
+			}
+
+		}
+	}
--- 第10回_ネスト/第10回_ネスト/TriangleTest.java
+++ 第10回_ネスト/第10回_ネスト/TriangleTest.java
@@ -2,11 +2,31 @@
 // 内側の条件が j < i（外側の i に連動）。何行・どんな形？
 class TriangleTest {
     public static void main(String[] args) {
+    	
+    	/*
         for (int i = 1; i <= 3; i++) {
             for (int j = 0; j < i; j++) {   // 内側の回数が i に連動
                 System.out.print("*");
             }
             System.out.println();
         }
+        
+     */
+    	
+    		for (int i = 5; i > 0; i--) {
+    			for (int j = 0; j < i; j++) {
+    				System.out.print("*");
+    			}
+    			System.out.println();
+    		}
+    		
+    		System.out.println();
+    		
+    		for (int i = 1; i <= 5; i++) {
+    			for (int j = 0; j < i; j++) {
+    				System.out.print("*");
+    			}
+    			System.out.println();
+    		}
     }
 }
